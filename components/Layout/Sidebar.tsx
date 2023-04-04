f08af8eef2704d411f0ffbaef2eaaba9b8aae19e
alex-candido <alex.candido.tec@gmail.com>
2023-04-04 11:02:21 -0300

    estrutura inicial component SidebarItem

--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -1,5 +1,7 @@
 import { BsBellFill, BsHouseFill } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
+
+import SidebarItem from "./SidebarItem";
 import SidebarLogo from "./SidebarLogo";
 
 const Sidebar = () => {
@@ -25,6 +27,14 @@ const Sidebar = () => {
       <div className="flex flex-col items-end">
         <div className="space-y-2 lg:w-[230px]">
           <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
         </div>
       </div>
     </div>
