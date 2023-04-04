469acded07811487217fe15179097a8d3c4772af
alex-candido <alex.candido.tec@gmail.com>
2023-04-04 10:18:14 -0300

    estrutura inicial component Sidebar

--- components/Layout/Sidebar.tsx
+++ components/Layout/Sidebar.tsx
@@ -1,4 +1,33 @@
+import { BsBellFill, BsHouseFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import SidebarLogo from "./SidebarLogo";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const items = [
+    {
+      icon: BsHouseFill,
+      label: "Home",
+      href: "/",
+    },
+    {
+      icon: BsBellFill,
+      label: "Notifications",
+      href: "/notifications",
+    },
+    {
+      icon: FaUser,
+      label: "Profile",
+      href: `/users/123`,
+    },
+  ];
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Sidebar;
