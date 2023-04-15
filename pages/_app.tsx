92b08ac80d03d5d9a7dc35a7b8082b252e8ea1e4
alex-candido <alex.candido.tec@gmail.com>
2023-04-15 20:37:49 -0300

    estrutura component components/modals/LoginModal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/Layout";
+import LoginModal from "@/components/modals/LoginModal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
@@ -6,6 +7,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       {/* <Modal actionLabel="submit" isOpen title="Test Modal"/> */}
+      <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
