9ae7001c69ea7d0e74aa1f0821210a72bb8542e0
alex-candido <alex.candido.tec@gmail.com>
2023-04-15 18:35:34 -0300

    estrutura component hooks/useLoginModal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import Layout from "@/components/Layout";
-import Modal from "@/components/Modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Modal actionLabel="submit" isOpen title="Test Modal"/>
+      {/* <Modal actionLabel="submit" isOpen title="Test Modal"/> */}
       <Layout>
         <Component {...pageProps} />
       </Layout>
