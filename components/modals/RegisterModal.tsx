5292a0976a632abe213b9258fc629b516af4dfb9
alex-candido <alex.candido.tec@gmail.com>
2023-04-15 22:54:33 -0300

    estrutura componet hooks/useRegisterModal e components/modals/RegisterModal

---
+++ components/modals/RegisterModal.tsx
@@ -0,0 +1,76 @@
+import { useCallback, useState } from "react";
+
+import useLoginModal from "@/hooks/useLoginModal";
+import useRegisterModal from "@/hooks/useRegisterModal";
+
+import Input from "../Input";
+import Modal from "../Modal";
+
+const RegisterModal = () => {
+  const loginModal = useLoginModal();
+  const registerModal = useRegisterModal();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      // TODO ADD LOG IN
+
+      loginModal.onClose();
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(true);
+    }
+  }, [loginModal]);
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Input
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+        disabled={isLoading}
+      />
+      <Input
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        disabled={isLoading}
+      />
+    </div>
+  )
+
+  return (
+    <Modal
+      disabled={isLoading}
+      isOpen={registerModal.isOpen}
+      title="Login"       
+      actionLabel="Sign in"
+      onClose={registerModal.onClose}
+      onSubmit={onSubmit}
+      body={bodyContent}
+    />
+  )
+}
+
+export default RegisterModal
