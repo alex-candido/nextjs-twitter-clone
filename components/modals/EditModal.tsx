d8dea85f857e183ac0092aae0d4401e4ebedecc2
alex-candido <alex.candido.tec@gmail.com>
2023-05-09 11:08:22 -0300

    adicionado estrutura inicial EditModal em handler em components/modals/EditModal.tsx

---
+++ components/modals/EditModal.tsx
@@ -0,0 +1,39 @@
+// import axios from "axios";
+import { useEffect, useState } from "react";
+// import { toast } from "react-hot-toast";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+import useEditModal from "@/hooks/useEditModal";
+import useUser from "@/hooks/useUser";
+
+// import Input from "../Input";
+// import Modal from "../Modal";
+// import ImageUpload from "../ImageUpload";
+
+const EditModal = () => {
+  const { data: currentUser } = useCurrentUser();
+  const { mutate: mutateFetchedUser } = useUser(currentUser?.id);
+  const editModal = useEditModal();
+
+  const [profileImage, setProfileImage] = useState('');
+  const [coverImage, setCoverImage] = useState('');
+  const [name, setName] = useState('');
+  const [username, setUsername] = useState('');
+  const [bio, setBio] = useState('');
+
+  useEffect(() => {
+    setProfileImage(currentUser?.profileImage)
+    setCoverImage(currentUser?.coverImage)
+    setName(currentUser?.name)
+    setUsername(currentUser?.username)
+    setBio(currentUser?.bio)
+  }, [currentUser?.name, currentUser?.username, currentUser?.bio, currentUser?.profileImage, currentUser?.coverImage]);
+
+
+
+  return (
+    <div></div>
+  )
+}
+
+export default EditModal;
