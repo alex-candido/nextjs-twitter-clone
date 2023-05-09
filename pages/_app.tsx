d1a281bcf59bd0b793541dccd77870d16c1ef06e
alex-candido <alex.candido.tec@gmail.com>
2023-05-09 10:26:52 -0300

    adicionado editModal em components/users/UserBio.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
-import Layout from "@/components/Layout";
-import LoginModal from "@/components/modals/LoginModal";
-import RegisterModal from "@/components/modals/RegisterModal";
-import "@/styles/globals.css";
 import { SessionProvider } from 'next-auth/react';
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app';
+
+import Layout from '@/components/Layout';
+import EditModal from '@/components/modals/EditModal';
+import LoginModal from '@/components/modals/LoginModal';
+import RegisterModal from '@/components/modals/RegisterModal';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
       <RegisterModal />
+      <EditModal />
       <LoginModal />
       <Layout>
         <Component {...pageProps} />
