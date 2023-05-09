044a26ad94e889d0e346d8b7f54b63c7ff64dc2a
alex-candido <alex.candido.tec@gmail.com>
2023-05-08 23:48:27 -0300

    adicionado estrutura UserHero em components/users/UserHero.tsx

--- components/users/UserHero.tsx
+++ components/users/UserHero.tsx
@@ -1,10 +1,31 @@
+import Image from "next/image";
+
+import useUser from "@/hooks/useUser";
+import Avatar from "../Avatar";
+
 interface UserHeroProps {
   userId: string;
 }
 
 const UserHero: React.FC<UserHeroProps> = ({ userId }) => {
+  const { data: fechedUser } = useUser(userId)
   return (
-    <><div>{userId}</div></>
+    <div>
+      <div
+        className="bg-neutral-700 h-44 relative"
+      >          
+        {fechedUser?.coverImage && (
+          <Image src={fechedUser.coverImage} 
+            fill 
+            alt="Cover Image"
+            style={{ objectFit: 'cover' }}
+          />
+        )}
+        <div className="absolute -bottom-16 left-4">
+          <Avatar userId={userId} isLarge hasBorder/>
+        </div>
+      </div>
+    </div>
   )
 }
 
