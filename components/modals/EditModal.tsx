0bab4152d8ab1ddcee14481c6408d5322a759d66
alex-candido <alex.candido.tec@gmail.com>
2023-05-09 23:38:19 -0300

    adicionado onSibmit em components/modals/EditModal.tsx

--- components/modals/EditModal.tsx
+++ components/modals/EditModal.tsx
@@ -1,13 +1,13 @@
-// import axios from "axios";
-import { useEffect, useState } from "react";
-// import { toast } from "react-hot-toast";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useEditModal from "@/hooks/useEditModal";
 import useUser from "@/hooks/useUser";
 
-// import Input from "../Input";
-// import Modal from "../Modal";
+import Input from "../Input";
+import Modal from "../Modal";
 // import ImageUpload from "../ImageUpload";
 
 const EditModal = () => {
@@ -29,10 +29,58 @@ const EditModal = () => {
     setBio(currentUser?.bio)
   }, [currentUser?.name, currentUser?.username, currentUser?.bio, currentUser?.profileImage, currentUser?.coverImage]);
 
+  const [isLoading, setIsLoading] = useState(false);
 
+  const onSubmit = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.patch('/api/edit', { name, username, bio, profileImage, coverImage });
+      mutateFetchedUser();
+
+      toast.success('Updated');
+
+      editModal.onClose();
+    } catch (error) {
+      toast.error('Something went wrong'); 
+    } finally {
+      setIsLoading(false);
+    }
+ }, [editModal, name, username, bio, mutateFetchedUser, profileImage, coverImage]);
+
+  const bodyContent = (
+    <div className="flex flex-col gap-4">
+      <Input
+        placeholder="Name"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+        disabled={isLoading}  
+      />
+      <Input 
+        placeholder="Username"
+        onChange={(e) => setUsername(e.target.value)}
+        value={username}
+        disabled={isLoading} 
+      />
+      <Input 
+        placeholder="Bio"
+        onChange={(e) => setBio(e.target.value)}
+        value={bio}
+        disabled={isLoading} 
+      />
+    </div>
+  )
 
   return (
-    <div></div>
+    <Modal
+      disabled={isLoading}
+      isOpen={editModal.isOpen}
+      title="Edit your profile"
+      actionLabel="Save"
+      onClose={editModal.onClose}
+      onSubmit={onSubmit}
+      body={bodyContent}
+    />
   )
 }
 
--- pages/api/edit.ts
+++ pages/api/edit.ts
@@ -4,11 +4,11 @@ import prisma from "@/libs/prismadb";
 import serverAuth from "@/libs/serverAuth";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-   if (req.method !== 'PATCH') {
+  if (req.method !== 'PATCH') {
     return res.status(405).end();
-   }
+  }
 
-   try {
+  try {
     const { currentUser } = await serverAuth(req);
 
     const { name, username, bio, profileImage, coverImage } = req.body;
@@ -19,7 +19,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     const updatedUser = await prisma.user.update({
       where: {
-        id: currentUser.id
+        id: currentUser.id,
       },
       data: {
         name,
@@ -31,7 +31,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
 
     return res.status(200).json(updatedUser);
-   } catch (error) {
+  } catch (error) {
     console.log(error);
     return res.status(400).end();
   }
