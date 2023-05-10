efca4b54a7aa16203c1323c27ef1f9100e087919
alex-candido <alex.candido.tec@gmail.com>
2023-05-10 14:52:05 -0300

    adicionado handleDrop em components/ImageUpload.tsx

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,6 +1,5 @@
 // import Image from "next/image";
-// import { useCallback, useState } from "react";
-// import { useDropzone } from "react-dropzone";
+import { useCallback, useState } from "react";
 
 interface DropzoneProps {
   onChange: (base64: string) => void;
@@ -10,6 +9,22 @@ interface DropzoneProps {
 }
 
 const ImageUpload: React.FC<DropzoneProps> = ({ onChange, label, value, disabled }) => {
+  const [base64, setBase64] = useState(value);
+
+  const handleChange = useCallback((base64: string) => {
+    onChange(base64)
+  }, [onChange])
+
+  const handleDrop = useCallback((files: any) => {
+    const file = files[0]
+    const render = new FileReader();
+
+    render.onload = (event: any) => {
+      setBase64(event?.target.result)
+      handleChange(event?.target.result)
+    }
+  }, [handleChange])
+
   return (
     <></>
   )
