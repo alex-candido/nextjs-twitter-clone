a43968330ed9016917f406f8f3ca01ecf4935334
alex-candido <alex.candido.tec@gmail.com>
2023-05-10 15:31:00 -0300

    adicionado estrutura ImageUpload em components/ImageUpload.tsx

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,5 +1,6 @@
-// import Image from "next/image";
+import Image from "next/image";
 import { useCallback, useState } from "react";
+import { useDropzone } from "react-dropzone";
 
 interface DropzoneProps {
   onChange: (base64: string) => void;
@@ -17,16 +18,43 @@ const ImageUpload: React.FC<DropzoneProps> = ({ onChange, label, value, disabled
 
   const handleDrop = useCallback((files: any) => {
     const file = files[0]
-    const render = new FileReader();
+    const reader = new FileReader();
 
-    render.onload = (event: any) => {
+    reader.onload = (event: any) => {
       setBase64(event?.target.result)
       handleChange(event?.target.result)
     }
+    reader.readAsDataURL(file);
   }, [handleChange])
 
+  const { getRootProps, getInputProps } = useDropzone({
+    maxFiles: 1,
+    onDrop: handleDrop,
+    disabled,
+    accept: {
+      'image/jpeg': [],
+      'image/png': []
+    }
+  })
+
   return (
-    <></>
+    <div {...getRootProps({className: 'w-full p-4 text-white text-center border-2 border-dotted rounded-md border-neutral-700 cursor-pointer hover:opacity-80'})}>
+      <input {...getInputProps()} />
+      {
+        base64 ? (
+          <div className="flex items-center justify-center">
+            <Image
+              src={base64}
+              height="100"
+              width="100"
+              alt="Uploaded image"
+            />
+          </div>
+        ) : (
+          <p className="text-white">{label}</p>
+        )
+      }
+    </div>
   )
 }
 
