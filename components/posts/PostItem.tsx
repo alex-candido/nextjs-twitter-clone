9d102e79693432b8b9d1fc8b2c523960640bfd52
alex-candido <alex.candido.tec@gmail.com>
2023-05-14 19:42:01 -0300

    adicionado funções goToUser, goToPost, onLike em components/posts/PostItem.tsx

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -1,10 +1,10 @@
-// import { useRouter } from 'next/router';
-// import { useCallback, useMemo } from 'react';
+import { useRouter } from 'next/router';
+import { useCallback } from 'react';
 // import { AiFillHeart, AiOutlineHeart, AiOutlineMessage } from 'react-icons/ai';
 // import { formatDistanceToNowStrict } from 'date-fns';
 
-// import useLoginModal from '@/hooks/useLoginModal';
-// import useCurrentUser from '@/hooks/useCurrentUser';
+import useCurrentUser from '@/hooks/useCurrentUser';
+import useLoginModal from '@/hooks/useLoginModal';
 // import useLike from '@/hooks/useLike';
 
 // import Avatar from '../Avatar';
@@ -14,6 +14,27 @@ interface PostItemProps {
 }
 
 const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
+  const router = useRouter();
+  const loginModal = useLoginModal();
+
+  const { data: currentUser } = useCurrentUser();
+
+  const goToUser = useCallback((event: any) => {
+    event.stopPropagation();
+    router.push(`/users/${data.user.id}`)
+  }, [router, data.user.id]);
+
+  const goToPost = useCallback((event: any) => {
+    event.stopPropagation();
+    router.push(`/posts/${data.id}`);
+  }, [router, data.id]);
+
+  const onLike = useCallback( async (event: any) => {
+    event.stopPropagation();
+
+    loginModal.onOpen();
+  }, [])
+
   return (
     <></>
   )
