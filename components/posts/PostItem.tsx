fed6fa928e45caec2fc777a2182724e478c59a77
alex-candido <alex.candido.tec@gmail.com>
2023-05-14 19:19:02 -0300

    adicionado estrutura PostItem em components/posts/PostItem.tsx

---
+++ components/posts/PostItem.tsx
@@ -0,0 +1,22 @@
+// import { useRouter } from 'next/router';
+// import { useCallback, useMemo } from 'react';
+// import { AiFillHeart, AiOutlineHeart, AiOutlineMessage } from 'react-icons/ai';
+// import { formatDistanceToNowStrict } from 'date-fns';
+
+// import useLoginModal from '@/hooks/useLoginModal';
+// import useCurrentUser from '@/hooks/useCurrentUser';
+// import useLike from '@/hooks/useLike';
+
+// import Avatar from '../Avatar';
+interface PostItemProps {
+  data: Record<string, any>;
+  userId?: string;
+}
+
+const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
+  return (
+    <></>
+  )
+}
+
+export default PostItem;
